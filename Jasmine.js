f0d7068d851d172337a58acd9b2514fb325ae467
Demo User <demouser@MacBook-Air.local>
2017-07-19 17:41:26 -0400

    added jquery to blog

---
+++ Jasmine.js
@@ -0,0 +1,17 @@
+function setup () {
+  $("#music").animate(
+    {"margin-left": "10px"},
+    5000
+  );
+  $("#photography").animate(
+    {"margin-left": "10px"},
+    4000
+  );
+  $("food").animate(
+    {"margin-left": "10px"},
+    3000
+  );
+  $("#shows").fadeIn(8000, "swing");
+}
+
+$(document).ready(setup);
